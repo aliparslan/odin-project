1d8efe16ffc1b0c4d17fefdf3dc370f8a3635315
Alip Arslan <aliparslan@outlook.com>
2021-12-24 20:20:59 -0500

    completed js exercises 1-7

--- foundations/js-exercises/02_repeatString/repeatString.js
+++ foundations/js-exercises/02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+    if (number < 0) {
+        return 'ERROR';
+    }
+    if (number == 0) {
+        return '';
+    }
+    return word + repeatString(word, number-1);
 };
-
+repeatString('hello', 2);
 // Do not edit below this line
 module.exports = repeatString;
--- foundations/js-exercises/07_tempConversion/tempConversion.js
+++ foundations/js-exercises/07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  if (temp == 32) {
+    return 0.0;
+  }
+  result = (temp - 32) * (5/9);
+  return Math.round(result * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  if (temp == 0) {
+    return 32.0;
+  }
+  result = 1.8 * temp + 32;
+  return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
